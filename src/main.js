ef66ffdd1c1068b10af1c4bb4537ec6f621afda6
jfgutierrezc <jgut13rr3zc@gmail.com>
2023-08-07 21:28:07 -0500

    Ya se realizan peticiones de back end a zabbix y se crea front end

--- src/main.js
+++ src/main.js
@@ -1,2 +1,35 @@
 
-console.log("API URL = " + API_KEY)
+//console.log("API URL = " + API_KEY)
+
+var url = "http://10.144.2.160/zabbix/api_jsonrpc.php";
+var bodyZ = {
+    "jsonrpc": "2.0",
+    "method": "item.get",
+    "params": {
+        "output": ["name","lastvalue","hostid","interfaceid","snmp_oid"],
+        "groupids":["76"],
+        "search": {
+            "name": ["tension"]
+        }
+    },
+    "auth": "d5aef56bf650141b17ee54a7f1e51bdc",
+    "id": 2
+};
+
+
+
+async function getItemZabbix(){
+const res = await fetch(url, {
+    method: "POST", 
+    body: JSON.stringify(bodyZ),
+    headers: {
+        "Content-Type": "application/json",
+    },
+    });
+const data = await res.json();
+const result = data.result[5]['name'];
+console.log({data,result})
+}
+
+
+getItemZabbix();
