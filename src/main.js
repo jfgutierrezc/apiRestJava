6ee739935c12ab48288bdb3cb7220bea3a847ed5
Juan Miguel <jmmunozpu@gmail.com>
2023-08-16 17:22:11 -0500

    Actualizacion en el js

--- src/main.js
+++ src/main.js
@@ -1,121 +1,70 @@
-const selectDepartamento = document.querySelector("#departamento")
-const selectMunicipio = document.querySelector("#municipio")
-let departamentos = []
-let data 
-let municipios = []
-let info = '/data/colombia.json'
+const selectDepartamento = document.querySelector("#departamento");
+const selectMunicipio = document.querySelector("#municipio");
+let departamentos = [];
+let data;
+let municipios = [];
+const info = '/data/colombia.json';
 
 document.addEventListener("DOMContentLoaded", async () => {
-    data = await obetenerDatos()
-    departamentos = data.map((dato)=>dato.departamento)
-    llenarDepartamentos()
-    selectDepartamento.addEventListener("change", obtenerMunicipios)
-   
-} )
+    data = await obtenerDatos();
+    departamentos = data.map(dato => dato.departamento);
+    llenarDepartamentos();
+    selectDepartamento.addEventListener("change", obtenerMunicipios);
+});
 
 window.onload = function () {
-    const host = document.getElementById("form_host");
-    const macro = document.getElementById("form_macros");
-    const inventory = document.getElementById("form_inventory");
-
-    host.style.display = "block";
-    macro.style.display = "none";
-    inventory.style.display = "none";
-
-
-
-}
-
-// Select anidados para departamento y municipios
-
-
-async function obetenerDatos(){
-    const respuesta = await fetch(info)
-    const departamentos = await respuesta.json()
-    return departamentos
-}
-
-
-function llenarDepartamentos(){
-    departamentos.forEach((departamento)=>{
-        const departamentoOption = document.createElement("option")
-        departamento.value = departamento
-        departamentoOption.textContent = departamento
-        selectDepartamento.appendChild(departamentoOption)
-    })
-}
-
-function obtenerMunicipios(event){
-    const departamento = event.target.value
-    
-    municipios = data.filter((dato)=> dato.departamento === departamento)[0].ciudades
-    llenarMunicipios()
-    
-    }
-
-function llenarMunicipios(){
-    while(selectMunicipio.firstChild){
-        selectMunicipio.firstChild.remove()
-    }
-    
-    municipios.forEach((nombre)=>{
-        const municipioOption = document.createElement("option")
-        municipioOption.value = nombre
-        municipioOption.textContent =  nombre
-        selectMunicipio.appendChild(municipioOption)
-    })
-
-}
-
+    mostrar("form_host");
+};
 
 function mostrar(id) {
-    const host = document.getElementById("form_host");
-    const macro = document.getElementById("form_macros");
-    const inventory = document.getElementById("form_inventory");
+    const formHost = document.getElementById("form_host");
+    const formMacros = document.getElementById("form_macros");
+    const formInventory = document.getElementById("form_inventory");
 
-    if (id === "form_host") {
-        host.style.display = "block";
-        macro.style.display = "none";
-        inventory.style.display = "none";
-    } else if (id === "form_macros") {
-        macro.style.display = "block";
-        host.style.display = "none";
-        inventory.style.display = "none";
-    } else {
-        host.style.display = "none";
-        macro.style.display = "none";
-        inventory.style.display = "block";
-    }
-
+    formHost.style.display = id === "form_host" ? "block" : "none";
+    formMacros.style.display = id === "form_macros" ? "block" : "none";
+    formInventory.style.display = id === "form_inventory" ? "block" : "none";
 }
 
-//Funcion que valida el formato de ip sea correcto
-function validaIp(ip) {
+function obtenerDatos() {
+    return fetch(info).then(response => response.json());
+}
 
+function llenarDepartamentos() {
+    selectDepartamento.innerHTML = departamentos
+        .map(departamento => `<option value="${departamento}">${departamento}</option>`)
+        .join("");
+}
+
+function obtenerMunicipios(event) {
+    const departamento = event.target.value;
+    municipios = data.find(dato => dato.departamento === departamento)?.ciudades || [];
+    llenarMunicipios();
+}
+
+function llenarMunicipios() {
+    selectMunicipio.innerHTML = municipios
+        .map(nombre => `<option value="${nombre}">${nombre}</option>`)
+        .join("");
+}
+
+function validaIp(ip) {
     const object = document.getElementById(ip);
     const valorInputIp = object.value;
+    const patronIp = /^([0-9]{1,3}).([0-9]{1,3}).([0-9]{1,3}).([0-9]{1,3})$/;
 
-    var patronIp = new RegExp("^([0-9]{1,3}).([0-9]{1,3}).([0-9]{1,3}).([0-9]{1,3})$");
-
-    if (valorInputIp.search(patronIp) == 0) {
-
-        valores = valorInputIp.split(".");
-        if (valores[0] <= 255 && valores[1] <= 255 && valores[2] <= 255 && valores[3] <= 255) {
-
+    if (patronIp.test(valorInputIp)) {
+        const valores = valorInputIp.split(".");
+        if (valores.every(val => parseInt(val) <= 255)) {
             return;
         }
     }
 
-    alert("Formato de IP incorrecto por favor verificar")
+    alert("Formato de IP incorrecto. Por favor verificar.");
 }
 
-
-//Funcion que valida que solo se digite numeros y el punto
 function soloNumeros() {
-    if ((event.keyCode != 46) && (event.keyCode < 48) || (event.keyCode > 57))
+    if ((event.keyCode !== 46) && (event.keyCode < 48) || (event.keyCode > 57)) {
         event.returnValue = false;
+    }
 }
-
-
-
-
