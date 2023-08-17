42c69d1f9b3f6ab377f45ab9db8c336700eb2e34
Juan Miguel <jmmunozpu@gmail.com>
2023-08-17 17:42:17 -0500

    Se realizan modificaciones en el formulario inventory para que sean campos obligatorios, que al seleccionar el departamento muestre los municipios asociados a el y que al seleccionar el municipio asocie la latitud y longitud

--- src/main.js
+++ src/main.js
@@ -8,7 +8,7 @@ const info = '/data/colombia.json';
 let selectedLatitud = "";
 let selectedLongitud = "";
 
-// Evento que se ejecuta cuando el contenido de la página ha cargado
+// Agrega el evento que se ejecutará cuando el contenido de la página haya cargado
 document.addEventListener("DOMContentLoaded", async () => {
     // Obtiene los datos del archivo JSON usando la función obtenerDatos()
     data = await obtenerDatos();
@@ -46,11 +46,11 @@ function obtenerDatos() {
 
 // Llena el select de departamentos con las opciones correspondientes
 function llenarDepartamentos() {
-  
-    selectDepartamento.innerHTML = departamentos
-        .map(departamento => `<option value="${departamento}">${departamento}</option>`)
-        .join(""); // Combina las opciones en una cadena para insertarlas en el select
-       
+    // Agrega la opción "Seleccionar" como un placeholder
+    selectDepartamento.innerHTML = `<option value="" disabled selected>Seleccione un departamento</option>` +
+        departamentos
+            .map(departamento => `<option value="${departamento}">${departamento}</option>`)
+            .join(""); // Combina las opciones en una cadena para insertarlas en el select
 }
 
 // Obtiene los municipios correspondientes al departamento seleccionado
@@ -61,14 +61,13 @@ function obtenerMunicipios(event) {
     // Llena el select de municipios con las opciones generadas
     llenarMunicipios();
 }
-
 // Llena el select de municipios con las opciones correspondientes
 function llenarMunicipios() {
-  
-    selectMunicipio.innerHTML = municipios
-        .map(nombre => `<option value="${nombre}">${nombre}</option>`)
-        .join(""); // Combina las opciones en una cadena para insertarlas en el select
-       
+    // Agrega la opción "Seleccionar" como un placeholder
+    selectMunicipio.innerHTML = `<option value="" disabled selected>Seleccione un municipio</option>` +
+        municipios
+            .map(nombre => `<option value="${nombre}">${nombre}</option>`)
+            .join(""); // Combina las opciones en una cadena para insertarlas en el select
 }
 
 // Valida el formato de una dirección IP
@@ -96,32 +95,32 @@ function soloNumeros() {
     }
 }
 
-
 // Esta función se encarga de guardar la latitud y longitud asociadas a una selección
-function guardarLatitudLongitud(event) {
+function guardarLatitudLongitud() {
     // Obtenemos el valor seleccionado en el campo de departamento
     const departamento = selectDepartamento.value;
-
     // Obtenemos el valor seleccionado en el campo de municipio
     const municipio = selectMunicipio.value;
-
     // Buscamos en los datos del JSON una entrada que coincida con el departamento y municipio seleccionados
     const seleccion = data.find(
-        (dato) => dato.departamento === departamento && dato.ciudades.includes(municipio)
+        (dato) => dato.departamento === departamento
     );
-
     // Si encontramos una selección válida
     if (seleccion) {
         // Obtenemos el índice del municipio seleccionado en el array de ciudades
         const index = seleccion.ciudades.indexOf(municipio);
-
-        // Guardamos la latitud y longitud correspondientes al municipio seleccionado
-        selectedLatitud = seleccion.latitud[index];
-        selectedLongitud = seleccion.longitud[index];
-
-        // Imprimimos la latitud y longitud en la consola para verificar
-        console.log(selectedLatitud);
-        console.log(selectedLongitud);
+        // Si encontramos la latitud y longitud correspondientes al municipio seleccionado
+        if (index !== -1) {
+            selectedLatitud = seleccion.latitudes[index];
+            selectedLongitud = seleccion.longitudes[index];
+            // Imprimimos la latitud y longitud en la consola para verificar
+            console.log(selectedLatitud);
+            console.log(selectedLongitud);
+        } else {
+            // Si no se encontraron latitud y longitud, reseteamos las variables
+            selectedLatitud = "";
+            selectedLongitud = "";
+        }
     } else {
         // Si no se encontró una selección válida, reseteamos las variables
         selectedLatitud = "";
